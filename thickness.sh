064635713a6d906d7e739d708ffdbc3b6ac2f32a
Peter Park <78362314+peterprk0@users.noreply.github.com>
2025-03-04 12:04:28 -0500

    Update thickness.sh

--- thickness.sh
+++ thickness.sh
@@ -10,4 +10,4 @@ echo "MEMB" | gmx convert-tpr -s md.tpr -o membrane.tpr -n groups.ndx
 echo "0" | gmx trjconv -f membrane_nj_centered.xtc -s membrane.tpr -o membrane_nj_centered_nobox.xtc -box 100 100 100
 
 # Calculating the membrane thickness
-echo -e "com of group 0 plus com of group 1" | gmx distance -f membrane_nj_centered_nobox.xtc -s md.tpr -oav thickness_IL.xvg -xvg xmgrace -n headgroups.ndx -b 0 -tu ns
+echo -e "com of group 0 plus com of group 1" | gmx distance -f membrane_nj_centered_nobox.xtc -s md.tpr -oav thickness.xvg -xvg xmgrace -n headgroups.ndx -b 0 -tu ns
